23cd287c57d55a369d857df6d253b165c755b6e0
Tucker2015 <mail@kevtucker.com>
2021-04-21 12:55:16 +0100

    - Add Tailwind

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,20 +1,12 @@
 import styles from '../../styles/Layout.module.css'
-
+import { Navbar } from './Navbar';
 
 const Layout = ({ children }) => {
 
 
     return (
         <>
-            <header className={styles.container}>
-                <h3 className={styles.bannerHeader}>Banner</h3>
-                <ul className={styles.list}>
-                    <li>Test</li>
-                    <li>Home</li>
-                    <li>Test</li>
-                    <li>Test</li>
-                </ul>
-            </header>
+            <Navbar />
             {children}
         </>
     )
